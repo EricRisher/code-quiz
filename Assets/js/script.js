3c542c6bb6c936d391da24e6cacc9eb2f52685c8
EricRisher <rishereric13@gmail.com>
2023-10-20 14:05:35 -0700

    buttons display correctly and proceed to next page

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,141 +1,206 @@
-let questionsEl = document.querySelector('#question-title');
-let timerEl = document.querySelector('#timer');
-let choicesEl = document.querySelector('#choices');
-let submitBtn = document.querySelector("#submitBtn");
-let startBtn = document.querySelector("#startBtn");
-let initialEl = document.querySelector("#initials");
-let feedbackEl = document.querySelector("#feedback");
-let choices = document.querySelector('#choices');
+const questionsEl = document.querySelector("#question-title");
+const timerEl = document.querySelector("#timer");
+const submitBtn = document.querySelector("#submitBtn");
+const startBtn = document.querySelector("#startBtn");
+const initialEl = document.querySelector("#initials");
+const feedbackEl = document.querySelector("#feedback");
+const choiceEl = document.querySelector("#choices");
+const correctStatusEl = document.querySelector("#correct");
 
 const questions = [
   {
     title: "Which one is a looping structure in JavaScript?",
-    choices: ["All the below", "For", "While", "Do-while loops"],
-    answer: "All the below",
+    choices: [
+      { text: "All the below", correct: true },
+      { text: "For", correct: false },
+      { text: "While", correct: false },
+      { text: "Do-while loops", correct: false },
+    ],
   },
   {
     title: "What are the two basic groups of data types in JavaScript?",
     choices: [
-      "Primitive and attribute",
-      "Primitive and reference types",
-      "Reference types and attribute",
-      "None of the above",
+      { text: "Primitive and attribute", correct: false },
+      { text: "Primitive and reference types", correct: true },
+      { text: "Reference types and attribute", correct: false },
+      { text: "None of the above", correct: false },
     ],
-    answer: "Primitive and reference types",
   },
   {
     title: "Commonly used data types DO NOT include:",
-    choices: ["strings", "booleans", "alerts", "numbers"],
-    answer: "alerts",
+    choices: [
+      { text: "strings", correct: false },
+      { text: "booleans", correct: false },
+      { text: "alerts", correct: true },
+      { text: "numbers", correct: false },
+    ],
   },
   {
     title: "Boolean operators that can be used in JavaScript include:",
     choices: [
-      "'And' Operator &&",
-      "'Or' Operator ||",
-      "'Not' Operator !",
-      "All the above",
+      { text: "'And' Operator &&", correct: false },
+      { text: "'Or' Operator ||", correct: false },
+      { text: "'Not' Operator !", correct: false },
+      { text: "All the above", correct: true },
     ],
-    answer: "All the above",
   },
   {
     title:
       "Which one of these is not among the three different types of errors in JavaScript?",
     choices: [
-      "Animation time errors",
-      "Load time errors",
-      "Run time errors",
-      "Logical Errors",
+      { text: "Animation time errors", correct: true },
+      { text: "Load time errors", correct: false },
+      { text: "Run time errors", correct: false },
+      { text: "Logical Errors", correct: false },
     ],
-    answer: "Animation time errors",
   },
   {
     title: "What is the data type of variables in JavaScript?",
     choices: [
-      "Object data types",
-      "Function data type",
-      "None of the above",
-      "All of the above",
+      { text: "Object data types", correct: false },
+      { text: "Function data type", correct: false },
+      { text: "None of the above", correct: false },
+      { text: "All of the above", correct: true },
     ],
-    answer: "Object data types",
   },
   {
     title: "The condition in an if / else statement is enclosed within ____.",
-    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-    answer: "parentheses",
+    choices: [
+      { text: "quotes", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "parentheses", correct: true },
+      { text: "square brackets", correct: false },
+    ],
   },
   {
     title: "Arrays in JavaScript can be used to store ____.",
     choices: [
-      "numbers and strings",
-      "other arrays",
-      "booleans",
-      "all of the above",
+      { text: "numbers and strings", correct: false },
+      { text: "other arrays", correct: false },
+      { text: "booleans", correct: false },
+      { text: "all of the above", correct: true },
     ],
-    answer: "all of the above",
   },
   {
     title:
       "String values must be enclosed within ____ when being assigned to variables.",
-    choices: ["commas", "curly brackets", "quotes", "parentheses"],
-    answer: "quotes",
+    choices: [
+      { text: "commas", correct: false },
+      { text: "curly brackets", correct: false },
+      { text: "quotes", correct: true },
+      { text: "parentheses", correct: false },
+    ],
   },
   {
     title:
       "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-    answer: "console.log",
+    choices: [
+      { text: "JavaScript", correct: false },
+      { text: "terminal / bash", correct: false },
+      { text: "for loops", correct: false },
+      { text: "console.log", correct: true },
+    ],
   },
   {
     title: "What is the type of Pop up boxes available in JavaScript?:",
-    choices: ["Alert", "Confirm", "Prompt", "All the above"],
-    answer: "All the above",
+    choices: [
+      { text: "Alert", correct: true },
+      { text: "Confirm", correct: true },
+      { text: "Prompt", correct: true },
+      { text: "All the above", correct: true },
+    ],
   },
 ];
 
+const penalty = 10;
+
+let score = 0;
 let questionIndex = 0;
 let time = questions.length * 15;
 let timerId;
+let selectBtn;
 
 function quizInit() {
-    //Hides main screen
-    let mainEl = document.querySelector(".main-screen");
-    mainEl.setAttribute("class", 'hide');
-    //Starts timer
-    timerId = setInterval(tick, 1000);
-    //Unhides questions
-    let questionsEl = document.querySelector("#quiz-questions");
-    questionsEl.classList.remove("hide");
-    displayQuestion();
+  let mainEl = document.querySelector(".main-screen");
+  mainEl.classList.add("hide");
+
+  timerId = setInterval(tick, 1000);
+  let questionsEl = document.querySelector("#quiz-questions");
+  questionsEl.classList.remove("hide");
+  displayQuestion();
 }
 
 function displayQuestion() {
   questionsEl.textContent = questions[questionIndex].title;
   displayChoices();
-};
+}
 
 function displayChoices() {
-  //clears previous choices
-  choices.innerHTML = '';
+  choiceEl.innerHTML = "";
 
-  for(let i = 0; i < questions[questionIndex].choices.length; i++){
-    const choiceBtn = document.createElement('button');
-    choiceBtn.textContent = `${i + 1}. ${questions[questionIndex].choices[i]}`;
-    choices.appendChild(choiceBtn);
+  const question = questions[questionIndex];
+
+  question.choices.forEach((choice, index) => {
+    const choiceBtn = document.createElement("button");
+    choiceBtn.innerHTML = `${index + 1}. ${choice.text}`;
+    choiceBtn.dataset.index = index;
+    choiceBtn.classList.add("choices");
+    choiceEl.appendChild(choiceBtn);
+
+    // Attach the click event listener for each choice button
+    choiceBtn.addEventListener("click", selectAnswer)
+  });
+}
+
+function selectAnswer(e) {
+  let selectBtn = e.target;
+      questionIndex++;
+
+  console.log(questionIndex, 158)
+  const isCorrect = questions.includes(true);
+  setStatusClass(isCorrect);
+  // Handle correct and incorrect answers
+  if (isCorrect) {
+    score += 1; // Increase score for correct answers
+  } else {
+    time -= penalty; // Deduct time for incorrect answers
   }
+  if (questionIndex < questions.length) {
+    displayQuestion();
+  } else {
+    quizEnd();
+  }
+  console.log(isCorrect)
 }
 
-function tick(){
-    time--;
-    timerEl.textContent = time;
-    time <= 0 ? quizEnd(): null;
+//let selectedChoice = questions[questionIndex].choices[selectBtn.dataset];
+
+function setStatusClass(isCorrect) {
+  clearStatusClass();
+  if (isCorrect) {
+    correctStatusEl.textContent = "Correct!";
+  } else {
+    correctStatusEl.textContent = "Wrong!";
+  }
+  correctStatusEl.classList.remove("hide");
 }
 
-function quizEnd(){
-    clearInterval(timerId);
+function clearStatusClass() {
+  correctStatusEl.classList.add("hide");
+  correctStatusEl.textContent = "";
+}
 
-    let endScreenEl = document.getElementById('#end-screen');
-    endScreenEl.removeAttribute('class');
+function tick() {
+  time--;
+  timerEl.textContent = `Time: ${time}`;
+
+  time <= 0 ? quizEnd() : null;
+}
+
+function quizEnd() {
+  clearInterval(timerId);
+  // Handle the end of the quiz, display score, and other actions
+  
 }
 
 startBtn.addEventListener("click", quizInit);
